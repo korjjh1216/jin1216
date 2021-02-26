7a75070919c7846e3d090c3844f1ad054fb0be0a
korjjh1216 <korjjh1216@naver.com>
2021-02-26 10:17:30 +0900

    2021-02-26 망가진 깃

---
+++ SPRING/jinhyeongcho/Homework/src/frontend/src/views/BoardList.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "BoardList"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ SPRING/jinhyeongcho/Homework/src/frontend/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <h2 align="center">홈화면입니다 게시판기능만 만드는중입니다. </h2>
+    <hr>
+    <div align="center" id="header">
+      <router-link :to="{ name: 'Home' }"
+                   class="nav-link"
+                   active-class="active">
+        Home
+      </router-link>
+      <router-link :to="{ name: 'About' }"
+                   class="nav-link"
+                   active-class="active">
+        About Us
+      </router-link>
+      <router-link :to="{ name: 'BoardListPage' }"
+                   class="nav-link"
+                   active-class="active">
+        게시판
+      </router-link>
+    </div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data: function (){
+    return{
+      message: "숙제중입니다.."
+    }
+  },
+  components: {
+  }
+}
+</script>
