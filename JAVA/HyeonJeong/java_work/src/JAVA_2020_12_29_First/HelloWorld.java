83c920945d928dee9589a7d3c12f4abfabf1f32a
korjjh1216 <76759810+korjjh1216@users.noreply.github.com>
2021-03-24 14:29:59 +0900

    Delete JAVA/HyeonJeong directory

--- JAVA/HyeonJeong/java_work/src/JAVA_2020_12_29_First/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package JAVA_2020_12_29_First;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println();
-    }
-}
