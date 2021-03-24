93a28c96116c7c3c718927fb8b6f6e2abe817b0d
korjjh1216 <76759810+korjjh1216@users.noreply.github.com>
2021-03-24 14:30:15 +0900

    Delete JAVA/Jinwook directory

--- JAVA/Jinwook/java_work/src/정기역략평가01/Exercise15.java
+++
@@ -1,9 +0,0 @@
-package 정기역략평가01;
-
-public class Exercise15 {
-    public static void main(String[] args) {
-        DementionalArr dr = new DementionalArr(2,2);
-        dr.mulArr();
-        dr.printArr();
-    }
-}
